10c841b6c7f0e490eff293f6f8854e54812815e4
EmaBord <emanuelborda@gmail.com.com>
2015-11-28 00:53:12 -0300

    listo la funcionalidad de filtro de gastos solo resta agregar a la vista

--- expenseManager/expense/urls.py
+++ expenseManager/expense/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns ('expenseManager.expense.views',
 			 url(r'expense/(?P<pk>[0-9]+)/$', ExpenseUpdate.as_view(), name='expense-update'),
     			 url(r'expense/(?P<pk>[0-9]+)/delete/$', ExpenseDelete.as_view(),  name='expense-delete'),
 			 url(r'^$', ExpenseListView.as_view(), name='expense-list'),
+			  url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]+)/$',ExpenseMonthArchiveView.as_view(month_format='%m'),name="expense_month_filter"),
 			)
--- expenseManager/expense/views.py
+++ expenseManager/expense/views.py
@@ -4,6 +4,24 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from expense.models import Expense
 
+from django.views.generic.dates import MonthArchiveView
+
+
+class ExpenseMonthArchiveView(MonthArchiveView):
+    queryset = Expense.objects.all()
+    date_field = "fecha"
+    allow_future = True
+
+    def get_context_data(self, **kwargs):
+        context = super(ExpenseMonthArchiveView, self).get_context_data(**kwargs)
+        total = 0
+        expenses = Expense.objects.all()
+        for e in expenses:
+            total = total + e.precio
+        context['total'] = total    
+        return context
+
+
 class ExpenseCreate(CreateView):
     model = Expense
     fields = ['descripcion','precio']  
@@ -24,10 +42,15 @@ class ExpenseListView(ListView):
     model = Expense
 
     def get_context_data(self, **kwargs):
+        import datetime
+        ahora = datetime.datetime.now()
         context = super(ExpenseListView, self).get_context_data(**kwargs)
         total = 0
-        expenses = Expense.objects.all()
+        expenses = Expense.objects.filter(fecha__year=ahora.year,fecha__month=ahora.month)
         for e in expenses:
             total = total + e.precio
         context['total'] = total    
+        context['object_list'] = expenses
         return context
+   
+
