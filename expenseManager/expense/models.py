8399e9f519758349b1400791cef09db97461ccc5
Ema Bord <emanuelborda@gmail.com>
2016-03-31 23:22:10 -0300

    datetime buenos aires

--- expenseManager/expense/models.py
+++ expenseManager/expense/models.py
@@ -2,9 +2,11 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
 # Create your models here.
-
+from pytz import timezone 
+from datetime import datetime
+buenos_aires = timezone('America/Argentina/Buenos_Aires')
 class Expense(models.Model):
-	fecha 			= models.DateField(default=timezone.now)
+	fecha 			= models.DateField(default=datetime.now(buenos_aires).replace(tzinfo=None))
 	descripcion 	= models.CharField(max_length =100)
 	precio			= models.FloatField() 
 	extraordinario	= models.IntegerField(choices = ((1, ("Si")),(0, ("No"))),default=0)
